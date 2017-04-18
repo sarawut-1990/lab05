1663fa2583c92e182f4ed448dd28280b2dedc95f
sarawut-1990 <sarawutturbo@gmail.com>
2017-04-18 15:23:06 +0700

    first Lab

---
+++ src/main/java/rmuti/lab05/LinkedListQueue.java
@@ -0,0 +1,40 @@
+package rmuti.lab05;
+
+public class LinkedListQueue {
+	private ArrayList lst;
+	public LinkedListQueue() {
+		lst = new ArrayList();
+	}
+
+	public void enqueue(Object e) {
+		lst.add(e);
+	}
+	
+	public Object peek() {
+        return lst.get(0);
+    }
+
+    public Object dequeue() {
+        Object obj = peek();
+        lst.remove(0);
+        return obj;
+    }
+
+	public static void main(String[] arg) {
+		LinkedListQueue s = new LinkedListQueue();
+		s.enqueue("a");
+		s.enqueue("b");
+		s.enqueue("c");
+		System.out.println(s);
+		System.out.println(s.peek());
+		System.out.println(s.dequeue());
+		System.out.println(s);
+		
+	}
+	
+	@Override
+	public String toString() {
+		return lst.toString();
+	}
+
+}
